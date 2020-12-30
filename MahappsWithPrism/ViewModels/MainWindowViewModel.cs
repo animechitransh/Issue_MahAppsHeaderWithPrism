90f42a771617c2e5307db57e7aa00fdc6bb2629f
Chitransh <animechitransh@gmail.com>
2020-12-31 00:56:13 +0530

    Code Check-in

---
+++ MahappsWithPrism/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace MahappsWithPrism.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+
+        #region Fields + Properties
+
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+
+        #endregion
+
+        #region Commands
+
+        private DelegateCommand _fieldName;
+        public DelegateCommand InWindowBtnCommand =>
+            _fieldName ?? (_fieldName = new DelegateCommand(ExecuteCommandName));
+
+        void ExecuteCommandName()
+        {
+            Message = "In Window Button Clicked!";
+        }
+
+        private DelegateCommand _titleCupcakeCommand;
+        public DelegateCommand TitleCupcakeCommand =>
+            _titleCupcakeCommand ?? (_titleCupcakeCommand = new DelegateCommand(ExecuteTitleCupcakeCommand));
+
+        void ExecuteTitleCupcakeCommand()
+        {
+            Message = "Cupcake in Window Header Clicked!";
+        }
+
+        private DelegateCommand _titleGHCommand;
+        public DelegateCommand TitleGHCommand =>
+            _titleGHCommand ?? (_titleGHCommand = new DelegateCommand(ExecuteTitleGHCommand));
+
+        void ExecuteTitleGHCommand()
+        {
+            Message = "GH in Window Header Clicked!";
+        }
+
+        #endregion
+
+        #region ctor
+        public MainWindowViewModel()
+        {
+
+        }
+
+        #endregion
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Issue_MahAppsHeaderWithPrism
  Demo of Prism Unable to Issue Commands from Window header buttons
+ -- Checked again--
+ NOT AN ISSUE, WORKING FINE
